1ff1acd3f8dddf1987ecbfb1e18c13bd89e5bf47
Augustin Estela <augustinestela@gmail.com>
2023-05-18 22:47:48 +0200

    chat et pong best

--- frontend/src/components/PongGame.vue
+++ frontend/src/components/PongGame.vue
@@ -1,23 +1,40 @@
 <template>
+
 <p>{{ "veloBall x-y " + veloBallX + " " + veloBallY }}</p>
 
 <div class="game-container">
   <div class="button-container">
-      <button @click="startMatchSolo" :disabled="gameIsRunning">Start</button>
-      <button @click="restartMatchSolo" :disabled="!gameIsRunning">Restart</button>
+    <button @click="startMatchSolo" :disabled="gameIsRunning">Start Solo</button>
+    <button @click="startMatchMultiLocal" :disabled="gameIsRunning">Start Multiplayer</button>
+    <button @click="restartMatch" :disabled="!gameIsRunning">Restart</button>
     </div>
 <div class="pong-container">
+
+    <div class="input-container">
+      <div class="left-input">
+        <p>{{ leftPlayerKeyUp }}</p>
+        <p>{{ leftPlayerKeyDown }}</p>
+      </div>
+    </div>
+
+  <div class="pong" :style="pongStyle">
   <div class="scores">
       <div class="scorePlayer">{{ "Score A : " + scoreA }}</div>
       <div class="scorePlayer">{{ "Score B : " + scoreB }}</div>
     </div>
-  <div class="pong">
-    
-    
+  
     <div class="paddle" :style="leftPaddleStyle"></div>
     <div class="paddle" :style="rightPaddleStyle"></div>
     <div class="ball" :style="ballStyle"></div>
   </div>
+
+  <div class="input-container">
+    <div class="right-input">
+        <p>{{ rightPlayerKeyUp }}</p>
+        <p>{{ rightPlayerKeyDown }}</p>
+      </div>
+    </div>
+
 </div>
 </div>
 
@@ -32,20 +49,20 @@ export default defineComponent({
     setup() {
 
 //GAME CONTAINER PARAMETERS
-const gameContainerWidth = 600;
-const gameContainerHeight = 400;
+const pongWidth = 600;
+const pongHeight = 400;
 
 //CONTROL PARAMETERS
-const rightPlayerKeyUp = 'ArrowUp';
-const rightPlayerKeyDown = 'ArrowDown';
-const leftPlayerKeyUp = 'w';
-const leftPlayerKeyDown = 's';
+const rightPlayerKeyUp = ref('ArrowUp');
+const rightPlayerKeyDown = ref('ArrowDown');
+const leftPlayerKeyUp = ref('w');
+const leftPlayerKeyDown = ref('s');
 
 //LEFT PADDLE PARAMETERS
 const leftPaddleWidth = ref(10);
 const leftPaddleHeight = ref(80);
 
-const leftPaddleY = ref(gameContainerHeight/2 - leftPaddleHeight.value/2);
+const leftPaddleY = ref(pongHeight/2 - leftPaddleHeight.value/2);
 const leftPaddleSpeed = ref(12);
 
 const leftPaddleJustHit = ref(false);
@@ -54,7 +71,7 @@ const leftPaddleJustHit = ref(false);
 const rightPaddleWidth = ref(10);
 const rightPaddleHeight = ref(80);
 
-const rightPaddleY = ref(gameContainerHeight/2 - rightPaddleHeight.value/2);
+const rightPaddleY = ref(pongHeight/2 - rightPaddleHeight.value/2);
 const rightPaddleSpeed = ref(12);
 
 const rightPaddleJustHit = ref(false);
@@ -63,8 +80,8 @@ const rightPaddleJustHit = ref(false);
 const ballSize = 10;
 const paddleOffset = 10;
 
-const ballX = ref(gameContainerWidth/2 - ballSize/2);
-const ballY = ref(gameContainerHeight/2 - ballSize/2);
+const ballX = ref(pongWidth/2 - ballSize/2);
+const ballY = ref(pongHeight/2 - ballSize/2);
 
 const ballStartSpeedX = -4;
 const ballStartSpeedY = 2;
@@ -98,20 +115,38 @@ const line = ref('');
 const startMatchSolo = () => {
   if (!gameIsRunning.value)
   {
-    veloBallX.value = ballStartSpeedX;
-    veloBallY.value = Math.random() * 3 - 6;
+    const rand = Math.random() * 2 - 1;
+    veloBallX.value = rand + (rand / Math.abs(rand)) * 2;
+    veloBallY.value = Math.random() * 6 - 3;
 
-    ballX.value = gameContainerWidth/2 - ballSize/2;
-    ballY.value = gameContainerHeight/2 - ballSize/2;
+    ballX.value = pongWidth/2 - ballSize/2;
+    ballY.value = pongHeight/2 - ballSize/2;
 
     rightPaddleHeight.value = 400;
     rightPaddleY.value = 0;
+
+    rightPlayerKeyDown.value = '';
+    rightPlayerKeyUp.value = '';
 
     gameIsRunning.value = true;
   }
 }
 
-const restartMatchSolo = () => {
+const startMatchMultiLocal = () => {
+  if (!gameIsRunning.value)
+  {
+    const rand = Math.random() * 2 - 1;
+    veloBallX.value = rand + (rand / Math.abs(rand)) * 2;
+    veloBallY.value = Math.random() * 6 - 3;
+
+    ballX.value = pongWidth/2 - ballSize/2;
+    ballY.value = pongHeight/2 - ballSize/2;
+
+    gameIsRunning.value = true;
+  }
+}
+
+const restartMatch = () => {
   if (gameIsRunning.value) {
     scoreA.value = 0;
     scoreB.value = 0;
@@ -119,10 +154,14 @@ const restartMatchSolo = () => {
     veloBallX.value = 0;
     veloBallY.value = 0;
 
-    
+    rightPaddleHeight.value = 80;
+    rightPaddleY.value = pongHeight/2 - rightPaddleHeight.value/2;
 
-    ballX.value = gameContainerWidth / 2 - ballSize / 2;
-    ballY.value = gameContainerHeight / 2 - ballSize / 2;
+    rightPlayerKeyDown.value = 'ArrowDown';
+    rightPlayerKeyUp.value = 'ArrowUp';
+
+    ballX.value = pongWidth / 2 - ballSize / 2;
+    ballY.value = pongHeight / 2 - ballSize / 2;
 
     gameIsRunning.value = false;
   }
@@ -133,12 +172,12 @@ const restartMatchSolo = () => {
 const moovePaddles = () => {
   if (leftArrowUp.value && leftPaddleY.value > 1 - leftPaddleHeight.value)
     leftPaddleY.value -= leftPaddleSpeed.value * leftArrowUp.value;
-  else if (leftArrowDown.value && leftPaddleY.value < gameContainerHeight - 1)
+  else if (leftArrowDown.value && leftPaddleY.value < pongHeight - 1)
     leftPaddleY.value += leftPaddleSpeed.value * leftArrowDown.value;
 
   if (rightArrowUp.value && rightPaddleY.value > 1 - rightPaddleHeight.value)
     rightPaddleY.value -= rightPaddleSpeed.value * rightArrowUp.value;
-  else if (rightArrowDown.value && rightPaddleY.value < gameContainerHeight - 1)
+  else if (rightArrowDown.value && rightPaddleY.value < pongHeight - 1)
     rightPaddleY.value += rightPaddleSpeed.value * rightArrowDown.value;
 }
 
@@ -148,24 +187,24 @@ const moovePaddles = () => {
 const ballWallColistion = () => {
   if (ballY.value <= 0 )
     veloBallY.value = Math.abs(veloBallY.value);
-  else if (ballY.value >= gameContainerHeight - ballSize)
+  else if (ballY.value >= pongHeight - ballSize)
   veloBallY.value = -Math.abs(veloBallY.value);
 
   if (ballX.value <= 1)
     {
       scoreB.value += 1;
-      ballX.value = gameContainerWidth/2 - ballSize/2;
-      ballY.value = gameContainerHeight/2 - ballSize/2;
+      ballX.value = pongWidth/2 - ballSize/2;
+      ballY.value = pongHeight/2 - ballSize/2;
       veloBallX.value = -ballStartSpeedX;
-      veloBallY.value = ballStartSpeedY;
+      veloBallY.value = (Math.random() * 6) - 3;
     }
-  if (ballX.value >= gameContainerWidth - ballSize - 1)
+  if (ballX.value >= pongWidth - ballSize - 1)
     {
       scoreA.value += 1;
-      ballX.value = gameContainerWidth/2 - ballSize/2;
-      ballY.value = gameContainerHeight/2 - ballSize/2;
+      ballX.value = pongWidth/2 - ballSize/2;
+      ballY.value = pongHeight/2 - ballSize/2;
       veloBallX.value = ballStartSpeedX;
-      veloBallY.value = -ballStartSpeedY;
+      veloBallY.value = (Math.random() * 6) - 3;
     }
 }
 
@@ -193,7 +232,7 @@ const gameLoop = () => {
 
 ballWallColistion();
 ballPaddleColision(paddleOffset + leftPaddleWidth.value, leftPaddleY.value,leftPaddleHeight.value, 1); 
-ballPaddleColision( (paddleOffset + rightPaddleWidth.value + ballSize) - gameContainerWidth, rightPaddleY.value, rightPaddleHeight.value, -1); 
+ballPaddleColision( (paddleOffset + rightPaddleWidth.value + ballSize) - pongWidth, rightPaddleY.value, rightPaddleHeight.value, -1); 
 
 
 
@@ -207,27 +246,27 @@ moovePaddles();
 //KEY HANDLING
 
 const handleKeyDown = (event: KeyboardEvent) => {
-  if (event.key === leftPlayerKeyUp) 
+  if (event.key === leftPlayerKeyUp.value) 
     leftArrowUp.value = 1;
-  else if (event.key === leftPlayerKeyDown)
+  else if (event.key === leftPlayerKeyDown.value)
     leftArrowDown.value = 1;
 
-  if (event.key === rightPlayerKeyUp )
+  if (event.key === rightPlayerKeyUp.value)
     rightArrowUp.value = 1;
-  else if (event.key === rightPlayerKeyDown )
+  else if (event.key === rightPlayerKeyDown.value)
     rightArrowDown.value = 1;
 };
 
 const handleKeyUp = (event: KeyboardEvent) => {
-  if (event.key === leftPlayerKeyUp) {
+  if (event.key === leftPlayerKeyUp.value) {
   leftArrowUp.value = 0;
-  } else if (event.key === leftPlayerKeyDown) {
+  } else if (event.key === leftPlayerKeyDown.value) {
   leftArrowDown.value = 0;
   }
 
-  if (event.key === rightPlayerKeyUp) {
+  if (event.key === rightPlayerKeyUp.value) {
   rightArrowUp.value = 0;
-  } else if (event.key === rightPlayerKeyDown) {
+  } else if (event.key === rightPlayerKeyDown.value) {
   rightArrowDown.value = 0;
   }
 };
@@ -246,6 +285,11 @@ const rightPaddleStyle = computed(() => ({
   right: `${paddleOffset}px`,
   '--paddle-width': `${rightPaddleWidth.value}px`,
   '--paddle-height': `${rightPaddleHeight.value}px`,
+}));
+
+const pongStyle = computed(() => ({
+  '--pongWidth': `${pongWidth}px`,
+  '--pongHeight': `${pongHeight}px`,
 }));
 
 const ballStyle = computed(() => ({
@@ -270,8 +314,14 @@ return {
   veloBallX,
   veloBallY,
   startMatchSolo,
-  restartMatchSolo,
+  restartMatch,
+  startMatchMultiLocal,
   gameIsRunning,
+  leftPlayerKeyUp,
+  leftPlayerKeyDown,
+  rightPlayerKeyUp,
+  rightPlayerKeyDown,
+  pongStyle,
 };
 
 
@@ -289,18 +339,6 @@ return {
 
 <style>
 
-.game-container {
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  display: flex;
-  align-items: center;
-  justify-content: center;
- 
-}
-
 .button-container {
   position: absolute;
   top: 0;
@@ -310,33 +348,62 @@ return {
   width: 100%;
 }
 
-.pong-container {
-  position: absolute;
-  top: 50%;
-  left: 25%;
-  transform: translate(-50%, -50%);
+.game-container {
+
+position :relative;
+display: flex;
+justify-content: center;
+align-items: center;
+height: 100vh;
 }
+
+.pong-container {
+display: flex;
+align-items: center;
+}
+
+.input-container {
+display: flex;
+justify-content: space-between;
+align-items: center;
+
+}
+
+.left-input,
+.right-input {
+width: 150px;
+}
+
+.left-input {
+text-align: right;
+}
+
+.right-input {
+text-align: left;
+}
+
+
 
 .pong {
   position: relative;
-  width: 600px;
-  height: 400px;
+  width: var(--pongWidth, 10px);
+  height: var(--pongHeight, 10px);
   border: 1px solid #6b4d4d;
   overflow: hidden;
 }
 
 .scores {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 40px;
-  font-size: 24px;
-  color: #09024b;
-  text-align: center;
+display: flex;
+justify-content: center;
+align-items: center;
+height: 40px;
+font-size: 24px;
+color: #09024b;
+text-align: center;
 }
 
 .scorePlayer {
-  flex: 1;
+flex: 1;
 }
 
 .paddle {
