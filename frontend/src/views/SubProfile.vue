0693428919e04b45accb8356c77991fac817cf93
tamigore <tadeo.amigo@gmail.com>
2023-08-11 18:01:09 +0200

    add back to front profile

--- frontend/src/views/SubProfile.vue
+++ frontend/src/views/SubProfile.vue
@@ -18,6 +18,7 @@
                   <span v-if="userData">{{ userData.username }}'s profile</span>
                 </div>
                 <div class="text-900 w-6 md:w-2 font-medium"></div>
+                <Button v-if="userData" @click="addFriend">Add Friend</Button>
               </li>
 
               <li class="flex align-items-center py-4 px-2 border-top-1 surface-border flex-wrap">
@@ -69,68 +70,80 @@ import { useRoute } from "vue-router";
 
 
 export default defineComponent({
-name: 'SubProfile',
-computed: {
-selectedImage() {
-  const selectedId = store.state.user.img;
-  return this.getImageById(selectedId) || { id: 1, img: require('@/assets/welc.jpeg') };
-},
-},
-async mounted() {
+  name: 'SubProfile',
+  computed: {
+    selectedImage() {
+      const selectedId = store.state.user.img;
+      return this.getImageById(selectedId) || { id: 1, img: require('@/assets/welc.jpeg') };
+    },
+  },
+  async mounted() {
     await this.getUserByUsername();
   },
-data() {
-  return {
-    userData: null as User | null,
-    storedUsername: '',
-    imageGrid: [
-      { id: "1", img: require('@/assets/profiles/profil_1.jpg') },
-      { id: "2", img: require('@/assets/profiles/profil_2.jpg') },
-      { id: "3", img: require('@/assets/profiles/profil_3.jpg') },
-      { id: "4", img: require('@/assets/profiles/profil_4.jpg') },
-      { id: "5", img: require('@/assets/profiles/profil_5.jpg') },
-      { id: "6", img: require('@/assets/profiles/profil_6.jpg') },
-      { id: "7", img: require('@/assets/profiles/profil_7.jpg') },
-      { id: "8", img: require('@/assets/profiles/profil_8.jpg') },
-      { id: "9", img: require('@/assets/profiles/profil_9.jpg') },
-    ],
-  }
-},
-methods: 
-{
-  getImageById(id: string | null) {
-    if (!id) return null;
-    return this.imageGrid.find(image => image.id === id);
+  data() {
+    return {
+      userData: null as User | null,
+      storedUsername: '',
+      imageGrid: [
+        { id: "1", img: require('@/assets/profiles/profil_1.jpg') },
+        { id: "2", img: require('@/assets/profiles/profil_2.jpg') },
+        { id: "3", img: require('@/assets/profiles/profil_3.jpg') },
+        { id: "4", img: require('@/assets/profiles/profil_4.jpg') },
+        { id: "5", img: require('@/assets/profiles/profil_5.jpg') },
+        { id: "6", img: require('@/assets/profiles/profil_6.jpg') },
+        { id: "7", img: require('@/assets/profiles/profil_7.jpg') },
+        { id: "8", img: require('@/assets/profiles/profil_8.jpg') },
+        { id: "9", img: require('@/assets/profiles/profil_9.jpg') },
+      ],
+    }
   },
+  methods:
+  {
+    getImageById(id: string | null) {
+      if (!id) return null;
+      return this.imageGrid.find(image => image.id === id);
+    },
 
-  async getUserByUsername() {
-    const route = useRoute();
-    const username = route.params.username;
-    console.log(username);
-    axios.defaults.baseURL = server.nestUrl;
-    return axios
-    .get(`/api/user/username/${username}`, {
-      headers: { Authorization: `Bearer ${store.state.user.hash}` },
-    })
-    .then((response: AxiosResponse) => {
-      console.log(response);
-      this.userData = response.data as User;
-    })
-    .catch((error: AxiosError) => {
-      console.error(error);
-    });
+    async getUserByUsername() {
+      const route = useRoute();
+      const username = route.params.username;
+      console.log(username);
+      axios.defaults.baseURL = server.nestUrl;
+      return axios
+        .get(`/api/user/username/${username}`, {
+          headers: { Authorization: `Bearer ${store.state.user.hash}` },
+        })
+        .then((response: AxiosResponse) => {
+          console.log(response);
+          this.userData = response.data as User;
+        })
+        .catch((error: AxiosError) => {
+          console.error(error);
+        });
+    },
+
+    async addFriend() {
+      axios.defaults.baseURL = server.nestUrl;
+      return axios
+        .post(`/api/user/friends/add`, this.userData, {
+          headers: { Authorization: `Bearer ${store.state.user.hash}` },
+        })
+        .then((response: AxiosResponse) => {
+          console.log(response);
+        })
+        .catch((error: AxiosError) => {
+          console.error(error);
+        });
+    },
   },
-},
 })
 </script>
 
 
 <style scoped>
-
-
 .selected-image {
   cursor: pointer;
-z-index: 1;
+  z-index: 1;
 }
 
 .selected-image.active {
@@ -138,32 +151,31 @@ z-index: 1;
 }
 
 .grid-container {
-display: grid;
-grid-template-columns: 32em;
-grid-gap: 1em;
+  display: grid;
+  grid-template-columns: 32em;
+  grid-gap: 1em;
 }
 
 .card {
-grid-column: 1;
+  grid-column: 1;
 }
 
 .selected-image {
-display: flex;
-justify-content: center;
-align-items: center;
-height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
 }
 
 .selected-image img {
-width: 500px;
-height: 500px;
-border-radius: 50%;
-object-fit: cover;
-padding: 30px;
+  width: 500px;
+  height: 500px;
+  border-radius: 50%;
+  object-fit: cover;
+  padding: 30px;
 }
 
 .profile-details {
-grid-column: 2;
+  grid-column: 2;
 }
-
 </style>
